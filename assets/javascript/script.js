221cda5399c46221df7be14e9f8032cc01f0cf3b
ajholsinger <ajholsinger.inc@gmail.com>
2018-07-17 00:40:55 -0400

    letters already guessed are displayed, alerted when won game.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,12 +2,14 @@ var guesses = 10;
 var wins = 0;
 var losses = 0;
 var lettersGuessed;
-var choices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var choices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 // When user presses any button, trigger computer event
 document.onkeyup = function(event) {
 //Computer selects letter
 	var userGuess = event.key;
+	var chances = document.getElementById("chances");
+	var guessed = document.getElementById("guessedLetters");
 	
 	var computerGuess = choices[Math.floor(Math.random() * choices.length)].toLocaleLowerCase();
 	console.log(computerGuess);
@@ -16,19 +18,21 @@ document.onkeyup = function(event) {
 	if (userGuess === computerGuess) {
 		wins++;
 		document.getElementById("wins").innerText = wins;
+		alert("You won!");
 	}
 //If user guesses incorrect, take a life
 	else {
+		guesses--;
+		chances.innerText = guesses;
+		guessed.innerText += userGuess + ", ";
 		if (guesses === 0) {
 			alert("You lost!");
 			losses++;
 			document.getElementById("losses").innerText = losses;
-			guesses = 11;
-			lettersGuessed;
+			guesses = 10;
+			chances.innerText = guesses;
+		guessed.innerText = "";
 		}
-		guesses--;
-		document.getElementById("chances").innerText = guesses;
-		document.getElementById("guessedLetters").innerText = userGuess;
 	}
 
 }
